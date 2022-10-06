beb0beb38a35134e0053f1495dfd362d1123a5f4
Vinicius-Alves02 <vinicius.vasques@sptech.school>
2022-10-06 20:42:10 -0300

    Teste looca

---
+++ PrototiposTelas-TesteLooca/teste-looca/src/main/java/com/mycompany/teste/looca/TesteLooca.java
@@ -0,0 +1,12 @@
+package com.mycompany.teste.looca;
+
+import com.github.britooo.looca.api.core.Looca;
+
+public class TesteLooca {
+
+    public static void main(String[] args) {
+    Looca looca = new Looca();
+        System.out.println(looca.getProcessador().getUso());
+        
+      
+}}
