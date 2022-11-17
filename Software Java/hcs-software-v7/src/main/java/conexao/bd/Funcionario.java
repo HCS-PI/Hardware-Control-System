9fea93e3f49b1145ee15d8ef514384ab2f653240
Vinicius-Alves02 <vinicius.vasques@sptech.school>
2022-11-16 21:41:35 -0300

    Att Java-v7

---
+++ Software Java/hcs-software-v7/src/main/java/conexao/bd/Funcionario.java
@@ -0,0 +1,72 @@
+
+package conexao.bd;
+
+
+public class Funcionario {
+    
+    private Integer id_funcionario;
+    private String nome_funcionario;
+    private String cpf;
+    private String email;
+    private String senha;
+    private String cargo;
+    private Integer fk_empresa;
+
+    public Integer getId_funcionario() {
+        return id_funcionario;
+    }
+
+    public void setId_funcionario(Integer id_funcionario) {
+        this.id_funcionario = id_funcionario;
+    }
+
+    public String getNome_funcionario() {
+        return nome_funcionario;
+    }
+
+    public void setNome_funcionario(String nome_funcionario) {
+        this.nome_funcionario = nome_funcionario;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public Integer getFk_empresa() {
+        return fk_empresa;
+    }
+
+    public void setFk_empresa(Integer fk_empresa) {
+        this.fk_empresa = fk_empresa;
+    }
+    
+    
+}
---
+++ Software Java/hcs-software-v7/src/main/java/hcs/coleta/dados/InserirDados.java
@@ -0,0 +1,41 @@
+
+
+
+
+
+package hcs.coleta.dados;
+
+import conexao.bd.DadosConexao;
+import conexao.bd.DadosConexaoAWS;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class InserirDados {
+    
+    DadosConexao database = new DadosConexao();
+
+    JdbcTemplate connection = database.getConnection();
+    
+    DadosConexaoAWS databaseAWS = new DadosConexaoAWS();
+
+    JdbcTemplate connectionAWS = databaseAWS.getConnection();
+    
+    public void inserirDadosCpu(){
+        DadosCpu dadosCpu = new DadosCpu();
+        
+        connection.update("insert into Medida(horario_registro,valor,fk_dispositivo) values (CURRENT_TIMESTAMP,"
+                + dadosCpu.getConsumoCpu() + ", 1);");
+        
+        connectionAWS.update("insert into Medida(horario_registro,valor,fk_dispositivo) values (now(),"
+                + dadosCpu.getConsumoCpu() + ", 1);");
+    }
+    public void inserirDadosRam(){
+        DadosMemoriaRam dadosRam = new DadosMemoriaRam();
+        
+        connection.update("insert into Medida(horario_registro,valor,fk_dispositivo) values (CURRENT_TIMESTAMP,"
+                + dadosRam.consumoRam + ", 3);");
+        
+    }
+    public void inserirDadosDisco(){
+    
+    }
+}
