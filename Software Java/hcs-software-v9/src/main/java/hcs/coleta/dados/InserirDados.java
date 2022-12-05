d422b5dabbca4e25fafec9250919906789e104c3
MattheusGomes <matheus.silva@sptech.school>
2022-12-05 19:06:30 -0300

    Pipe vai?

--- Software Java/hcs-software-v9/src/main/java/hcs/coleta/dados/InserirDados.java
+++ Software Java/hcs-software-v9/src/main/java/hcs/coleta/dados/InserirDados.java
@@ -13,32 +13,51 @@ public class InserirDados {
     DadosConexaoAWS databaseAWS = new DadosConexaoAWS();
 
     JdbcTemplate connectionAWS = databaseAWS.getConnection();
+    Pipefy pipefy = new Pipefy();
 
     public void inserirDadosCpu() {
         DadosCpu dadosCpu = new DadosCpu();
-
+        String descricaoCpu;
         connection.update("insert into Medida(horario_registro,valor,fk_dispositivo) values (CURRENT_TIMESTAMP,"
                 + dadosCpu.getConsumoCpu() + ", 5);");
 
         connectionAWS.update("insert into Medida(horario_registro,valor,fk_dispositivo) values (now(),"
                 + dadosCpu.getConsumoCpu() + ", 5);");
+
+        if (dadosCpu.getConsumoCpu() > 70 && dadosCpu.getConsumoCpu() < 90) {
+            descricaoCpu = String.format("O carro está com a cpu em %.1f%%", dadosCpu.getConsumoCpu());
+            pipefy.criarCards("Novo carro em estado de alerta", descricaoCpu);
+        } else if (dadosCpu.getConsumoCpu() > 90) {
+            descricaoCpu = String.format("O carro está com a cpu em %.1f%%", dadosCpu.getConsumoCpu());
+            pipefy.criarCards("Novo carro em estado CRITICO", descricaoCpu);
+        }
     }
 
     public void inserirDadosRam() {
+        String descricaoRam;
         DadosMemoriaRam dadosRam = new DadosMemoriaRam();
+        System.out.println(dadosRam.getConsumoRam());
 
         connection.update("insert into Medida(horario_registro,valor,fk_dispositivo) values (CURRENT_TIMESTAMP,"
                 + dadosRam.getConsumoRam() + ", 7);");
 
         connectionAWS.update("insert into Medida(horario_registro,valor,fk_dispositivo) values (now(),"
                 + dadosRam.getConsumoRam() + ", 7);");
+
+        if (dadosRam.getConsumoRam() > 70 && dadosRam.getConsumoRam() < 90) {
+            descricaoRam = String.format("O carro está com a ram em %.1f%%", dadosRam.getConsumoRam());
+            pipefy.criarCards("Novo carro em estado de alerta", descricaoRam);
+        } else if (dadosRam.getConsumoRam() > 90) {
+            descricaoRam = String.format("O carro está com a ram em %.1f%%", dadosRam.getConsumoRam());
+            pipefy.criarCards("Novo carro em estado CRITICO", descricaoRam);
+        }
     }
 
     public void inserirDadosDisco() {
         // Em desenvolvimento
     }
 
-    public void inserirDadosTemperatura(){
+    public void inserirDadosTemperatura() {
         DadosTemperatura dadosTemp = new DadosTemperatura();
 
         connection.update("insert into Medida(horario_registro,valor,fk_dispositivo) values (CURRENT_TIMESTAMP,"
