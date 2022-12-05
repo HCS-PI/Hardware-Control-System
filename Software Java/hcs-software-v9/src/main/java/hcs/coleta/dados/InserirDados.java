de8235835482070e4c2cb2fbb9d778d6dbaaa096
RicardoTeixeira2004 <ricardo.santos@sptech.school>
2022-12-05 14:30:31 -0300

    v9 denovo

--- Software Java/hcs-software-v9/src/main/java/hcs/coleta/dados/InserirDados.java
+++ Software Java/hcs-software-v9/src/main/java/hcs/coleta/dados/InserirDados.java
@@ -13,57 +13,38 @@ public class InserirDados {
     DadosConexaoAWS databaseAWS = new DadosConexaoAWS();
 
     JdbcTemplate connectionAWS = databaseAWS.getConnection();
-    Pipefy pipefy = new Pipefy();
 
     public void inserirDadosCpu() {
-        String descricaoCpu;
         DadosCpu dadosCpu = new DadosCpu();
 
         connection.update("insert into Medida(horario_registro,valor,fk_dispositivo) values (CURRENT_TIMESTAMP,"
-                + dadosCpu.getConsumoCpu() + ", 5);");
+                + dadosCpu.getConsumoCpu() + ", 1);");
 
         connectionAWS.update("insert into Medida(horario_registro,valor,fk_dispositivo) values (now(),"
-                + dadosCpu.getConsumoCpu() + ", 5);");
-
-        if (dadosCpu.getConsumoCpu() > 70 && dadosCpu.getConsumoCpu() < 90) {
-            descricaoCpu = String.format("O carro está com a cpu em %.1f%%", dadosCpu.getConsumoCpu());
-            pipefy.criarCards("Novo carro em estado de alerta", descricaoCpu);
-        } else if (dadosCpu.getConsumoCpu() > 90) {
-            descricaoCpu = String.format("O carro está com a cpu em %.1f%%", dadosCpu.getConsumoCpu());
-            pipefy.criarCards("Novo carro em estado CRITICO", descricaoCpu);
-        }
+                + dadosCpu.getConsumoCpu() + ", 1);");
     }
 
     public void inserirDadosRam() {
-        String descricaoRam;
         DadosMemoriaRam dadosRam = new DadosMemoriaRam();
 
         connection.update("insert into Medida(horario_registro,valor,fk_dispositivo) values (CURRENT_TIMESTAMP,"
-                + dadosRam.getConsumoRam() + ", 7);");
+                + dadosRam.getConsumoRam() + ", 3);");
 
         connectionAWS.update("insert into Medida(horario_registro,valor,fk_dispositivo) values (now(),"
-                + dadosRam.getConsumoRam() + ", 7);");
-
-        if (dadosRam.getConsumoRam() > 70 && dadosRam.getConsumoRam() < 90) {
-            descricaoRam = String.format("O carro está com a ram em %.1f%%", dadosRam.getConsumoRam());
-            pipefy.criarCards("Novo carro em estado de alerta", descricaoRam);
-        } else if (dadosRam.getConsumoRam() > 90) {
-            descricaoRam = String.format("O carro está com a ram em %.1f%%", dadosRam.getConsumoRam());
-            pipefy.criarCards("Novo carro em estado CRITICO", descricaoRam);
-        }
+                + dadosRam.getConsumoRam() + ", 3);");
     }
 
     public void inserirDadosDisco() {
         // Em desenvolvimento
     }
 
-    public void inserirDadosTemperatura() {
+    public void inserirDadosTemperatura(){
         DadosTemperatura dadosTemp = new DadosTemperatura();
 
         connection.update("insert into Medida(horario_registro,valor,fk_dispositivo) values (CURRENT_TIMESTAMP,"
-                + dadosTemp.getTemperaturaCelsius() + ", 6);");
+                + dadosTemp.getTemperaturaCelsius() + ", 2);");
 
         connectionAWS.update("insert into Medida(horario_registro,valor,fk_dispositivo) values (now(),"
-                + dadosTemp.getTemperaturaCelsius() + ", 6);");
+                + dadosTemp.getTemperaturaCelsius() + ", 2);");
     }
 }
